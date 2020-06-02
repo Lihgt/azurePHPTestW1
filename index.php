74db7083d84aee6593327ed86959ac44c2ea9ae8
Lihgt <wamr@mail.ru>
2020-06-02 19:13:49 +0300

    3

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
-echo "Hello World!".PHP_EOL.PHP_EOL;
+echo "Hello World!".PHP_EOL.PHP_EOL."<br/>";
 
-echo 'HTTPS: '.$_SERVER['HTTPS'].PHP_EOL;
-echo 'Addr: '.$_SERVER['REMOTE_ADDR'].PHP_EOL;
-echo 'User: '.$_SERVER['REMOTE_USER'].PHP_EOL;
-echo 'Method: '.$_SERVER['REQUEST_METHOD'].PHP_EOL;
-echo 'PHP-User: '.$_SERVER['PHP_AUTH_USER'].PHP_EOL;
-echo 'Auth: '.$_SERVER['AUTH_TYPE'].PHP_EOL;
-echo 'PostUser:'.$_POST['username'].PHP_EOL;
-echo 'RequestUser:'.$_REQUEST['username'].PHP_EOL;
-echo get_current_user().PHP_EOL;
-echo 'Host: '.gethostbyaddr($_SERVER['REMOTE_ADDR']);
+echo 'HTTPS: '.$_SERVER['HTTPS'].PHP_EOL."<br/>";
+echo 'Addr: '.$_SERVER['REMOTE_ADDR'].PHP_EOL."<br/>";
+echo 'User: '.$_SERVER['REMOTE_USER'].PHP_EOL."<br/>";
+echo 'Method: '.$_SERVER['REQUEST_METHOD'].PHP_EOL."<br/>";
+echo 'PHP-User: '.$_SERVER['PHP_AUTH_USER'].PHP_EOL."<br/>";
+echo 'Auth: '.$_SERVER['AUTH_TYPE'].PHP_EOL."<br/>";
+echo 'PostUser:'.$_POST['username'].PHP_EOL."<br/>";
+echo 'RequestUser:'.$_REQUEST['username'].PHP_EOL."<br/>";
+echo get_current_user().PHP_EOL."<br/>";
+echo 'Host: '.gethostbyaddr($_SERVER['REMOTE_ADDR'])."<br/>";
