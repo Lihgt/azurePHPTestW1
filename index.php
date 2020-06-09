3049d208c9bf051d753978e95549453287f7200d
Lihgt <wamr@mail.ru>
2020-06-09 19:04:09 +0300

    html

--- index.php
+++ index.php
@@ -1,17 +1,67 @@
 <?php
 
-echo "Hello World!".PHP_EOL.PHP_EOL."<br/>";
+echo "<h3>Добрый день!</h3>
+<br>
+<br>
 
-echo 'HTTPS: '.$_SERVER['HTTPS'].PHP_EOL."<br/>";
-echo 'Addr: '.$_SERVER['REMOTE_ADDR'].PHP_EOL."<br/>";
-echo 'User: '.$_SERVER['REMOTE_USER'].PHP_EOL."<br/>";
-echo 'Method: '.$_SERVER['REQUEST_METHOD'].PHP_EOL."<br/>";
-echo 'PHP-User: '.$_SERVER['PHP_AUTH_USER'].PHP_EOL."<br/>";
-echo 'Auth: '.$_SERVER['AUTH_TYPE'].PHP_EOL."<br/>";
-echo 'PostUser:'.$_POST['username'].PHP_EOL."<br/>";
-echo 'RequestUser:'.$_REQUEST['username'].PHP_EOL."<br/>";
-echo get_current_user().PHP_EOL."<br/>";
-echo 'Host: '.gethostbyaddr($_SERVER['REMOTE_ADDR'])."<br/>";
+<script type=\"text/javascript\">
+var sysInfo    = new ActiveXObject(\"ADSystemInfo\");
+var us = sysInfo.UserName;
 
-echo 'REQUEST:'.$_REQUEST['ax_name'];
-echo 'POST:'.$_POST['ax_name'];
+showUsers();
+
+function GetFieldValue(objField){
+    var result = \"\";
+    if (objField.Value == null){
+    }
+    else{
+        if (objField.type == 12){
+          var ldapArr = objField.Value.toArray();
+          result = ldapArr[0];
+        }
+        else{
+          result = objField.Value;
+        }
+    }
+    return result;
+}//GetFieldValue
+
+function showUsers(){
+  var strAttributes, strFilter, SrchCriteria, strQuery;
+
+  strAttributes = \"cn,sn,title,mail,mobile,telephoneNumber\";
+  SrchCriteria=\"(sAMAccountType=805306368)\";
+
+  var ADOConnection = new ActiveXObject(\"ADODB.Connection\");
+  var ADOCommand = new ActiveXObject(\"ADODB.Command\");
+  var ADORecordSet = new ActiveXObject(\"ADODB.RecordSet\");
+  ADOConnection.Provider = \"ADsDSOObject\";
+  strBase = \"<LDAP://\" + us + \">\";
+
+try {
+  ADOConnection.Open(\"ADs Provider\");
+  ADOCommand.ActiveConnection = ADOConnection;
+  strFilter = SrchCriteria;
+  strQuery = strBase + \";\" + strFilter + \";\" + strAttributes + \";subtree\";
+  console.log(strQuery);
+
+  ADOCommand.CommandText = strQuery;
+  ADORecordSet = ADOCommand.Execute();
+
+  if (ADORecordSet.RecordCount > 0) {
+    ADORecordSet.MoveFirst;
+    while (! ADORecordSet.EOF){
+      console.log (\"title: \"+GetFieldValue(ADORecordSet.Fields(\"title\")));
+      console.log (\"name: \"+GetFieldValue(ADORecordSet.Fields(\"cn\")));
+      console.log (\"mail: \"+GetFieldValue(ADORecordSet.Fields(\"mail\")));
+      console.log (\"mobile: \"+GetFieldValue(ADORecordSet.Fields(\"mobile\")));
+	    console.log (\"phone: \"+GetFieldValue(ADORecordSet.Fields(\"telephoneNumber\")));
+      ADORecordSet.MoveNext;
+    }
+  }//if 
+  ADORecordSet.Close();
+} catch(e) {  console.log (e.message); } 
+} //showUsers
+
+</script>"
+?>
